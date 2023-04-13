40b4b457c69467bca08dcd34c563532ab47463f2
gracerabano <gracearabano@gmail.com>
2023-04-13 00:53:43 -0500

    Home content done (about me)

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="bg-no-repeat bg-cover bg-center bg-fixed bg-[url(/blue-lace.svg)] min-h-screen">
-        <div className="container grid grid-cols-12 gap-10 justify-center mx-auto px-16 py-16 w-full">
+        <div className="container grid grid-cols-12 gap-10 justify-center mx-auto my-auto px-16 py-16 w-full">
           {/* left */}
           <div className="container col-span-4">
             <div className={`mt-32 sticky flex mx-auto items-center justify-center backdrop-blur-sm bg-sheer rounded-lg px-16 py-16 transform transition-all duration-1000 delay-500 ${isLoaded ? "translate-x-0 opacity-100 fixed" : "-translate-x-10 opacity-0"}`}>
@@ -40,12 +40,21 @@ export default function Home() {
                 )}
               </header>
 
-              <div className={`mt-10 items-center justify-center w-full h-screen backdrop-blur-sm bg-sheer rounded-lg px-16 py-16 transform transition-all duration-1000 delay-500 ${isLoaded ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"}`}>
+              <div className={`flex flex-col mt-10 justify-start w-full backdrop-blur-sm bg-sheer rounded-lg px-16 py-16 transform transition-all duration-1000 delay-500 ${isLoaded ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"}`}>
                 {isLoaded && (
                   <>
-                    <h1 className="text-5xl font-bold font-serif">Hello!</h1>
-                    <p className="text-lg mt-2 px-2 text-gray-700">
-                      My name is Grace Rabano.{" "}
+                    <h1 className="inline-block text-left px-2 text-4xl font-bold mr-16">Hello! Nice to Meet You!</h1>
+                    <p className="block text-lg mt-4 mr-16 px-2 text-gray-700 font-serif leading-7">
+                      My name is Grace Rabano. I'm currently studying Computer Science at the University of Houston. 
+                      I'm discovering my passion for web development and I'm eager to learn more about front-end development and design.
+                      Though my focus is web dev, I'm also interested in branching out into other fields. 
+                      At my core, I'm a problem solver and my aim is to help solve complex problems with simple solutions, whether that be through code or otherwise.
+                    </p>
+                    <p className="block text-lg mt-4 mr-16 px-2 text-gray-700 font-serif leading-7">
+                      Outside of class, you will find me with my nose buried in a book, folding origami, and petting every cat I see.
+                    </p>
+                    <p className="block text-lg mt-4 mr-16 px-2 text-gray-700 font-serif leading-7">
+                      Thanks for stopping by!
                     </p>
                   </>
                 )}
